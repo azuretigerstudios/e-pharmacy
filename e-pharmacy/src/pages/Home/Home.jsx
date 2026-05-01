26769858daf75d2d5e84daf8a3c1ae9a09c00974
azuretigerstudios <azuretigerstudios@gmail.com>
2026-05-01 23:26:55 +0300

    Create Home.jsx

---
+++ e-pharmacy/src/pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+// Home.jsx örneği
+const Home = () => {
+  return (
+    <main>
+      <h1>Welcome to E-Pharmacy</h1>
+      {/* Hero, MedicineStore vb. buraya gelecek */}
+    </main>
+  );
+};
+export default Home;
